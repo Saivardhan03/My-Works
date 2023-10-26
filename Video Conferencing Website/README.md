6679e8ce86682b448917d1671ffd6b53b561289f
Saivardhan03 <143811051+Saivardhan03@users.noreply.github.com>
2023-10-27 01:51:48 +0530

    Add files via upload

---
+++ Video Conferencing Website/README.md
@@ -0,0 +1,4 @@
+# Video Conferencing Website
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Saivardhan-Revuri/pen/MWLavNq](https://codepen.io/Saivardhan-Revuri/pen/MWLavNq).
+
---
+++ Video Conferencing Website/src/script.js
@@ -0,0 +1,11 @@
+const localVideo = document.getElementById('local-video');
+const remoteVideo = document.getElementById('remote-video');
+
+navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    .then(function (stream) {
+        localVideo.srcObject = stream;
+
+    })
+    .catch(function (error) {
+        console.error('getUserMedia error:', error);
+    });
